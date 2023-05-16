9a1c443c822fd8628308c1bdc84c69f3f6fdb089
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-05-16 14:04:00 +0200

    chat - socket.io

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const clients = [];
 wss.on("connection", (socket) => {
   
   clients.push(socket);
-  // console.log("new client connected");
-  
   for (const client of clients) {
     if (client === socket) {
       
@@ -19,7 +17,7 @@ wss.on("connection", (socket) => {
   }
   socket.on("message", (message) => {
     const data = JSON.parse(message.toString());
-    console.log(data)
+    
     for (const client of clients){
       if (client === socket){
         client.send(`You: ${data.message}. ${dayjs().format("HH:mm:ss DD/MM/YYYY")}`);
@@ -29,4 +27,4 @@ wss.on("connection", (socket) => {
   }
   });
 });
-console.log("server started");
+console.log("server started 9090");
---
+++ socket.io/server2.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const {Server} = require("socket.io");
+const dayjs = require("dayjs");
+const server = http.createServer();
+const io = new Server(server, {
+  cors:{
+    origin: "*",
+  }
+});
+
+io.on("connection", (socket) => {
+  socket.emit("chatMessage", "Welcome to chat");
+  socket.broadcast.emit("chatMessage", `New user connected,  ${dayjs().format("HH:mm:ss DD/MM/YYYY")}`
+  );
+
+  socket.on("chatMessage", (message) => {
+  const data = JSON.parse(message.toString());
+  socket.emit("chatMessage", `You: ${data.message}. ${dayjs().format("HH:mm:ss DD/MM/YYYY")}`
+  );
+    socket.broadcast.emit("chatMessage", `${data.name}: ${data.message}. ${dayjs().format("HH:mm:ss DD/MM/YYYY")}`);
+  }
+);
+});
+
+server.listen(9090, () => {
+  console.log("Server 9090");
+})
+
+
